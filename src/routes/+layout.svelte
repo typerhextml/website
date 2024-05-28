10b4f41c4bb1b6b56c91cdc2765782c811d37394
Leonard Lesinski <leoqonline@gmail.com>
2024-05-28 21:33:40 +0200

    Added mobile start page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,31 @@
 <script lang="ts">
 	import '../styles/global.scss';
 
-  import { BookText, Menu } from 'lucide-svelte';
+	import { BookText, Menu } from 'lucide-svelte';
 </script>
 
 <div class="h-16 w-lvw bg-blue-900"></div>
 <nav
-	class="fixed left-[0.5rem] top-2 h-12 w-[calc(100vw-1rem)] rounded-lg border-2 border-blue-600 bg-blue-950 bg-opacity-50 text-white backdrop-blur-sm flex p-2 items-center"
+	class="fixed left-[0.5rem] top-2 flex h-12 w-[calc(100vw-1rem)] items-center rounded-lg border-2 border-blue-600 bg-blue-950 bg-opacity-50 text-white backdrop-blur-sm"
 >
-  <a href="https://github.com/typerhextml/typerhext" class="size-fit hover:text-blue-500 transition">
-    <BookText class="size-6" />
-  </a>
-  <div class="flex-grow"></div>
-	<header>
-		<a href="/" class="mono text-3xl font-black h-fit w-fit block pb-1">TyperHext</a>
+	<a
+		href="https://github.com/typerhextml/typerhext"
+		class="size-fit transition hover:text-blue-500 p-2"
+	>
+		<BookText class="size-6" />
+	</a>
+	<div class="flex-grow"></div>
+	<header class="block h-full pt-1">
+		<a
+			href="/"
+			class="mono block h-full w-fit text-3xl font-black transition hover:text-blue-500"
+			>Typer<span class="text-white transition pr-2 ml-1 border-x-white border-x hover:border-x-transparent">Hext</span></a
+		>
 	</header>
-  <div class="flex-grow"></div>
-  <label for="menu-open" class="size-fit hover:text-blue-500 transition">
-    <Menu class="size-6" />
-  </label>
+	<div class="flex-grow"></div>
+	<label for="menu-open" class="size-fit transition hover:text-blue-500 p-2">
+		<Menu class="size-6" />
+	</label>
 </nav>
 <main class="min-h-[calc(100vh-4rem)] bg-blue-900">
 	<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,60 @@
+<h1 class="pt-20">Finally, a good preprocessor for HTML!</h1>
+
+<br />
+
+<div class="flex justify-center gap-4 pb-10">
+	<a href="https://github.com/typerhextml/typerhext" class="button main"> Get started </a>
+	<a href="https://github.com/typerhextml/typerhext" class="button"> Read the docs </a>
+</div>
+
+<br />
+
+<code lang="th">
+	<span class="font-black">=== My new website ===</span><br />
+	Welcome to my <i>__new__</i> website!<br />
+	It's completely written in TyperHext,<br />
+	the <b>**best**</b> preprocessor for HTML!<br />
+	<br />
+	Check it out<br />
+	<span class="text-blue-400">[]</span><span class="bg-blue-500 px-1">here</span><span
+		class="text-blue-400">[<span class="text-white">/</span>]</span
+	><span class="text-blue-400">@</span><span
+		class="italic underline decoration-blue-500 decoration-2">https://typerhext.com/</span
+	><span class="text-blue-400">@</span>!
+</code>
+
+<p class="py-4 text-center text-xl">would result in the following HTML code:</p>
+
+<code lang="html">
+	<span class="text-blue-400">&lt;<span class="text-white">h1</span>&gt;</span>My new website<span
+		class="text-blue-400">&lt;<span class="text-white">/h1</span>&gt;</span
+	><br />
+	<span class="text-blue-400">&lt;<span class="text-white">p</span>&gt;</span><br />
+	&nbsp;&nbsp;Welcome to my
+	<span class="text-blue-400">&lt;<span class="text-white">i</span>&gt;</span>new<span
+		class="text-blue-400">&lt;<span class="text-white">/i</span>&gt;</span
+	>
+	website!<br />
+	&nbsp;&nbsp;It's completely written in TyperHext,<br />
+	&nbsp;&nbsp;the
+	<span class="text-blue-400">&lt;<span class="text-white">b</span>&gt;</span>best<span
+		class="text-blue-400">&lt;<span class="text-white">/b</span>&gt;</span
+	>
+	preprocessor for HTML!<br />
+	<span class="text-blue-400">&lt;<span class="text-white">/p</span>&gt;</span><br />
+	<span class="text-blue-400">&lt;<span class="text-white">p</span>&gt;</span><br />
+	&nbsp;&nbsp;Check it out
+	<span class="text-blue-400"
+		>&lt;<span class="text-white">a</span> <span class="text-white">href</span>=<span
+			class="text-blue-400">"https://typerhext.com/"</span
+		>&gt;</span
+	>here<span class="text-blue-400">&lt;<span class="text-white">/a</span>&gt;</span>!<br />
+	<span class="text-blue-400">&lt;<span class="text-white">/p</span>&gt;</span>
+</code>
+
+<p class="pt-4 text-lg">
+	As you see, TyperHext has no keywords. Just symbols, so you just need to learn the very easy
+	syntax, and therefore a lot easier than HTML.<br /><br />In fact, you can learn TyperHext
+	completely in two hours and
+</p>
+<p class="pb-4 text-2xl text-blue-400 text-center">change the code you'll write forever!</p>
